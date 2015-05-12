54f65cc68925314e2335f831c2e2b19824caf45b
Dimitris Bozelos <dbozelos@gmail.com>
2015-05-12 09:24:19 +0000

    render admin pages using bootstrap grid (columns in a row)

--- template.php
+++ template.php
@@ -130,3 +130,95 @@ function bootstrap_admin_admin_block_content($variables) {
 
   return $output;
 }
+
+/**
+ * Overrides theme_admin_page().
+ *
+ * Render admin pages using Bootstrap Grid rows and columns.
+ */
+function bootstrap_admin_admin_page($variables) {
+  $blocks = $variables['blocks'];
+
+  $stripe = 0;
+  $container = array('left' => '', 'right' => '');
+
+  // Position blocks in left and right columns.
+  foreach ($blocks as $block) {
+    $block_output = theme('admin_block', array('block' => $block));
+    if (!$block_output) {
+      continue;
+    }
+
+    if (empty($block['position'])) {
+      // Perform automatic striping.
+      $block['position'] = ++$stripe % 2 ? 'left' : 'right';
+    }
+
+    $container[$block['position']] .= $block_output;
+  }
+
+  // Render all output as columns within a row.
+  $output = '<div class="row">';
+  $output .= '<div class="col-xs-12">' . theme('system_compact_link') . '</div>';
+
+  foreach ($container as $id => $data) {
+    $output .= '<div class="col-md-6">';
+    $output .= $data;
+    $output .= '</div>';
+  }
+
+  $output .= '</div>';
+
+  return $output;
+}
+
+/**
+ * Overrides theme_system_admin_index().
+ *
+ * Render the admin index page using Bootstrap Grid rows and columns.
+ */
+function bootstrap_admin_system_admin_index($variables) {
+  $menu_items = $variables['menu_items'];
+
+  $stripe = 0;
+  $container = array('left' => '', 'right' => '');
+  $flip = array('left' => 'right', 'right' => 'left');
+  $position = 'left';
+
+  // Iterate over all modules.
+  foreach ($menu_items as $module => $block) {
+    list($description, $items) = $block;
+
+    // Output links.
+    if (count($items)) {
+      $block = array();
+      $block['title'] = $module;
+      $block['content'] = theme('admin_block_content', array('content' => $items));
+      $block['description'] = t($description);
+      $block['show'] = TRUE;
+
+      if ($block_output = theme('admin_block', array('block' => $block))) {
+        if (!isset($block['position'])) {
+          // Perform automatic striping.
+          $block['position'] = $position;
+          $position = $flip[$position];
+        }
+        $container[$block['position']] .= $block_output;
+      }
+    }
+  }
+
+  // Render all output as columns within a row.
+  $output = '<div class="row">';
+  $output .= '<div class="col-xs-12">' . theme('system_compact_link') . '</div>';
+
+  foreach ($container as $id => $data) {
+    $output .= '<div class="col-md-6">';
+    $output .= $data;
+    $output .= '</div>';
+  }
+
+  $output .= '</div>';
+
+  return $output;
+}
